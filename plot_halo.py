1f876cdbf306074f29bb99f1cbd8474d6a99846c
UzTak <49874360+UzTak@users.noreply.github.com>
2023-06-04 16:55:00 -0700

    update

--- plot_halo.py
+++ plot_halo.py
@@ -10,11 +10,41 @@ from propagate import propagate_cr3bp, propagate_cr3bp_with_STM
 from plot_inv_manifold import plot_inv_manifold
 
 
+
+
+def set_axes_equal(ax):
+    '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
+    cubes as cubes, etc..  This is one possible solution to Matplotlib's
+    ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
+
+    Input
+      ax: a matplotlib axis, e.g., as output from plt.gca().
+    '''
+
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    x_range = abs(x_limits[1] - x_limits[0])
+    x_middle = np.mean(x_limits)
+    y_range = abs(y_limits[1] - y_limits[0])
+    y_middle = np.mean(y_limits)
+    z_range = abs(z_limits[1] - z_limits[0])
+    z_middle = np.mean(z_limits)
+
+    # The plot bounding box is a sphere in the sense of the infinity
+    # norm, hence I call half the max range the plot radius.
+    plot_radius = 0.5*max([x_range, y_range, z_range])
+
+    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
+
 # set-up of problem
-mu = 3.04036e-6
-Az_km = 200000
-lstar = 1.49598e8
-fam = 1
+mu = 1.215058560962404E-2
+Az_km = 6000
+lstar = 389703
+fam = 3  # north
 lp = 2
 
 # initial guess of Halo Orbit: Lindstedt-Poincare Method
@@ -35,10 +65,12 @@ l1, l2, l3, l4, l5 = find_lagrangian_pts(mu)
 
 # Plot the orbits
 fig = plt.figure()
-ax = plt.axes(projection='3d')
+# ax = plt.axes(projection='3d')
+ax = plt.axes()
+
 
 # plot invariant manifolds
-fig, ax = plot_inv_manifold(fig, ax, mu, sopt_ini, period_opt, eps='fix', arc_num=20, t_prop=5)
+fig, ax = plot_inv_manifold(fig, ax, mu, sopt_ini, period_opt, eps='fix', arc_num=20, t_prop=4.3)
 
 
 # ax.plot3D(s0[0,:], s0[1,:], s0[2,:], color='gray', label='initial guess')
@@ -46,11 +78,15 @@ fig, ax = plot_inv_manifold(fig, ax, mu, sopt_ini, period_opt, eps='fix', arc_nu
 # ax.scatter(s0_ini[0], s0_ini[1], s0_ini[2], color='red')
 # ax.plot3D(pos[:,0], pos[:,1], pos[:,2], color='blue', label='accurate')
 # ax.plot3D(pos_[:,0], pos_[:,1], pos_[:,2], color='purple', label='test')
-ax.scatter(l2[0], l2[1], l2[2], marker='*', color='orange', label='L2')
+# ax.scatter(l2[0], l2[1], l2[2], marker='*', color='orange', label='L2')
+ax.scatter(l2[0], l2[1], marker='d', color='orange', s=20, label='L2')
+# set_axes_equal(ax)
+ax.axis("equal")
+ax.grid(visible=True, which='both')
 
-
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
+ax.set_xlabel('x, LU')
+ax.set_ylabel('y, LU')
+# ax.set_zlabel('z')
 plt.legend()
+fig.tight_layout()
 plt.show()
--- plot_inv_manifold.py
+++ plot_inv_manifold.py
@@ -6,10 +6,10 @@ from Lindstedt_Poincare import initial_guess_halo, find_lagrangian_pts
 from optimize_halo import optimize_halo
 from propagate import propagate_cr3bp, propagate_cr3bp_with_STM
 
-def plot_inv_manifold(fig, ax, mu, s0, period, eps='fix', arc_num=10, t_prop=100):
+def plot_inv_manifold(fig, ax, mu, s0, period, eps='fix', arc_num=10, t_prop=100, d=2):
 
     if eps == 'fix':
-        eps = 1e-6
+        eps = 1 * 10**(-4)
 
     # 1. create monodromy matrix (propagating STM)
     (_, _, _, monodromy) = propagate_cr3bp_with_STM(s0, mu, period)
@@ -21,10 +21,10 @@ def plot_inv_manifold(fig, ax, mu, s0, period, eps='fix', arc_num=10, t_prop=100
     for j in range(len(w)):
         if w[j].real > 1 and w[j].imag == 0:
             wu = w[j].real
-            vu = v[:,j].real
+            vu = v[:, j].real
         if w[j].real < 1 and w[j].imag == 0:
             ws = w[j].real
-            vs = v[:,j].real
+            vs = v[:, j].real
 
     # 3. translate eigensystems -> make multiple starting point
     t_list = np.linspace(0, period, arc_num)
@@ -32,8 +32,8 @@ def plot_inv_manifold(fig, ax, mu, s0, period, eps='fix', arc_num=10, t_prop=100
     for i in range(arc_num):
         t = t_list[i]
         (pos_t, vel_t, _, stm_t) = propagate_cr3bp_with_STM(s0, mu, t, step=round(t/0.01)+2)
-        vu_t = np.dot(stm_t, vu)
         vs_t = np.dot(stm_t, vs)
+        vu_t = np.dot(stm_t, vu)
 
         xi = np.reshape([pos_t[-1,:], vel_t[-1,:]], (-1))
 
@@ -44,16 +44,24 @@ def plot_inv_manifold(fig, ax, mu, s0, period, eps='fix', arc_num=10, t_prop=100
         xsi2 = xi - eps * vs_t / np.linalg.norm(vs_t)
 
         # 5. progagate the state with perturbation
-        (pos_u1, vel_u1) = propagate_cr3bp(xui1, mu, t_prop, step=100)
-        (pos_u2, vel_u2) = propagate_cr3bp(xui2, mu, t_prop, step=100)
-        (pos_s1, vel_s1) = propagate_cr3bp(xsi1, mu, -t_prop, step=100)
-        (pos_s2, vel_s2) = propagate_cr3bp(xsi2, mu, -t_prop, step=100)
+        (pos_u1, vel_u1) = propagate_cr3bp(xui1, mu, t_prop, step=200)
+        (pos_u2, vel_u2) = propagate_cr3bp(xui2, mu, t_prop, step=200)
+        (pos_s1, vel_s1) = propagate_cr3bp(xsi1, mu, -t_prop, step=200)
+        (pos_s2, vel_s2) = propagate_cr3bp(xsi2, mu, -t_prop, step=200)
 
-        ax.plot3D(pos_u1[:,0], pos_u1[:,1], pos_u1[:,2], color='brown', linewidth=1)
-        ax.plot3D(pos_u2[:,0], pos_u2[:,1], pos_u2[:,2], color='red', linewidth=1)
-        ax.plot3D(pos_s1[:,0], pos_s1[:,1], pos_s1[:,2], color='lime', linewidth=1)
-        ax.plot3D(pos_s2[:,0], pos_s2[:,1], pos_s2[:,2], color='green', linewidth=1)
-        # ax.scatter(pos_s1[0,0], pos_s1[0,1], pos_s1[0,2], color='lime', marker="x")
-        # ax.scatter(pos_s2[0,0], pos_s2[0,1], pos_s2[0,2], color='green', marker="x")
+        if d == 3:
+            # ax.plot3D(pos_u1[:,0], pos_u1[:,1], pos_u1[:,2], color='brown', linewidth=1)
+            ax.plot3D(pos_u2[:,0], pos_u2[:,1], pos_u2[:,2], color='red', linewidth=1)
+            ax.plot3D(pos_s1[:,0], pos_s1[:,1], pos_s1[:,2], color='lime', linewidth=1)
+            # ax.plot3D(pos_s2[:,0], pos_s2[:,1], pos_s2[:,2], color='green', linewidth=1)
+            # ax.scatter(pos_s1[0,0], pos_s1[0,1], pos_s1[0,2], color='lime', marker="x")
+            # ax.scatter(pos_s2[0,0], pos_s2[0,1], pos_s2[0,2], color='green', marker="x")
+        elif d == 2:
+            # ax.plot(pos_u1[:,0], pos_u1[:,1], pos_u1[:,2], color='brown', linewidth=1)
+            ax.plot(pos_u2[:, 0], pos_u2[:, 1], color='red', linewidth=1)
+            ax.plot(pos_s1[:, 0], pos_s1[:, 1], color='lime', linewidth=1)
+            # ax.plot(pos_s2[:,0], pos_s2[:,1], pos_s2[:,2], color='green', linewidth=1)
+            # ax.scatter(pos_s1[0,0], pos_s1[0,1], pos_s1[0,2], color='lime', marker="x")
+            # ax.scatter(pos_s2[0,0], pos_s2[0,1], pos_s2[0,2], color='green', marker="x")
 
     return fig, ax
